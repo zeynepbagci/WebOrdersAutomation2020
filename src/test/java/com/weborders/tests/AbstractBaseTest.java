1a5cb863494fdeb6035c7360e12a0b24d1f5cf2b
zeynepbagci <zenepozcan@gmail.com>
2020-04-05 15:49:32 -0400

    commit

--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -13,6 +13,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
+import java.io.IOException;
+
 public abstract class AbstractBaseTest {
 
     protected WebDriver driver = Driver.getDriver();
@@ -51,7 +53,20 @@ public abstract class AbstractBaseTest {
     public void teardown(ITestResult testResult) {
         if (testResult.getStatus() == ITestResult.FAILURE) {
             String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
+            try {
+                extentTest.fail(testResult.getName());//test name that failed
+                extentTest.addScreenCaptureFromPath(screenshotLocation);//screen shot for evidence
+                extentTest.fail(testResult.getThrowable());//error message
+            }catch(IOException e){
+                e.printStackTrace();
+                throw  new RuntimeException("Failed to attach screenshot");
+            }
+        }else if(testResult.getStatus() == ITestResult.SUCCESS){
+            extentTest.pass(testResult.getName());
+        }else if(testResult.getStatus() == ITestResult.SKIP){
+            extentTest.skip(testResult.getName());
         }
+
         BrowserUtilities.wait(3);
         Driver.closeDriver();
     }
