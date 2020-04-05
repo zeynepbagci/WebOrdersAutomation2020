b5a92747c500024e9c68b3177ae8e545ef0afe80
zeynepbagci <zenepozcan@gmail.com>
2020-04-05 15:37:09 -0400

    commit

--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -1,4 +1,58 @@
 package com.weborders.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.weborders.utilities.BrowserUtilities;
+import com.weborders.utilities.ConfigurationReader;
+import com.weborders.utilities.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
 public abstract class AbstractBaseTest {
+
+    protected WebDriver driver = Driver.getDriver();
+
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest() {
+        extentReports = new ExtentReports();
+        String reportPath = "";
+
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            reportPath = System.getProperty("user.dir") + "\\test-output\\report.html";
+        } else {
+            reportPath = System.getProperty("user.dir") + "/test-output/report.html";
+        }
+        extentHtmlReporter = new ExtentHtmlReporter(reportPath);
+        extentReports.attachReporter(extentHtmlReporter);
+        extentHtmlReporter.config().setReportName("WebOrders Automation");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        extentReports.flush();
+    }
+
+    @BeforeMethod
+    public void setup() {
+        driver.get(ConfigurationReader.getProperty("url"));
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown(ITestResult testResult) {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
+        }
+        BrowserUtilities.wait(3);
+        Driver.closeDriver();
+    }
 }
